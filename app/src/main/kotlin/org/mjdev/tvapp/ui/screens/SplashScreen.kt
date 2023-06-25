c4ee3afc307aa10784171e6afd947e56638a3eb3
Milan Jurkulak <mimoccc@gmail.com>
2023-06-26 00:17:08 +0200

    splash screen menu removal

--- app/src/main/kotlin/org/mjdev/tvapp/ui/screens/SplashScreen.kt
+++ app/src/main/kotlin/org/mjdev/tvapp/ui/screens/SplashScreen.kt
@@ -8,24 +8,14 @@
 
 package org.mjdev.tvapp.ui.screens
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
-import org.mjdev.tvapp.R
 import org.mjdev.tvapp.base.annotations.TvPreview
 import org.mjdev.tvapp.base.navigation.MenuItem
 import org.mjdev.tvapp.base.navigation.Screen
 
 class SplashScreen : Screen() {
-
-    override val titleResId = R.string.app_name
-
-    override val menuTitleResId: Int = R.string.app_name
-
-    override val menuIcon: ImageVector get() = Icons.Filled.Home
 
     @TvPreview
     @Composable
