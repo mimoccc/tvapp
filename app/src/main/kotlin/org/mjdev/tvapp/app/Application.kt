7a9d60346b4471987e5c7aee23d428f21b1f5397
Milan Jurkulak <mimoccc@gmail.com>
2023-12-04 20:55:05 +0100

    code improvements

--- app/src/main/kotlin/org/mjdev/tvapp/app/Application.kt
+++ app/src/main/kotlin/org/mjdev/tvapp/app/Application.kt
@@ -9,11 +9,9 @@
 package org.mjdev.tvapp.app
 
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.Dispatchers
 import org.mjdev.tvapp.sync.SyncService.Companion.createSyncAccount
 import org.mjdev.tvapp.sync.SyncService.Companion.requestSync
 import org.mjdev.tvlib.application.TvApplication
-import org.mjdev.tvlib.extensions.GlobalExt.launch
 
 @HiltAndroidApp
 class Application : TvApplication() {
