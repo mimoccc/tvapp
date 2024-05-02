c657166a232f2de1bee389e1fda0ccbe9ed6bc82
milan jurkulák <mimoccc@gmail.com>
2024-05-02 15:00:10 +0200

    documentation

--- scripts/update-wiki.sh
+++ scripts/update-wiki.sh
@@ -7,27 +7,29 @@
 #  w: https://mjdev.org
 #
 set -eu
-TEMP_REPO_DIR="wiki_action_$GITHUB_REPOSITORY$GITHUB_SHA"
-TEMP_WIKI_DIR="temp_wiki_$GITHUB_SHA"
+#
+TEMP_REPO_DIR="$GITHUB_REPOSITORY_$GITHUB_SHA"
+#
 if [ -z "$WIKI_DIR" ]; then
     echo "Wiki location is not specified, using default wiki/"
     WIKI_DIR='wiki'
 fi
+#
 if [ -z "$GH_TOKEN" ]; then
     echo "Token is not specified"
     exit 1
 fi
 # Clone wiki repo
 echo "Cloning wiki repo https://github.com/$GITHUB_REPOSITORY.wiki.git"
-cd "$TEMP_REPO_DIR"
-git clone "https://$GITHUB_ACTOR:$GH_TOKEN@github.com/$GITHUB_REPOSITORY.wiki.git" "$TEMP_WIKI_DIR"
+#
+git clone "https://$GITHUB_ACTOR:$GH_TOKEN@github.com/$GITHUB_REPOSITORY.wiki.git" "$TEMP_REPO_DIR"
+# Copy wikis
+echo "Copying edited wiki"
+cp -R "$WIKI_DIR/" "$TEMP_REPO_DIR"
 # Get commit details
 author=`git log -1 --format="%an"`
 email=`git log -1 --format="%ae"`
 message=`git log -1 --format="%s"`
-# Copy wikis
-echo "Copying edited wiki"
-cp -R "$WIKI_DIR/" "$TEMP_WIKI_DIR"
 # Changes check
 echo "Checking if wiki has changes"
 cd "$TEMP_WIKI_DIR"
